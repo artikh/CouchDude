235b83b5fa2a043cf1871ccbc81fa112b06dcf44
Artem Tikhomirov <artem@tikhomirovs.ru>
2011-04-19 14:04:55 +0400

    TypeConvention

---
+++ CouchDude.Core/Conventions/ITypeConvention.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace CouchDude.Core.Conventions
+{
+	/// <summary>Detects document type for given entities.</summary>
+	/// <remarks>Implementers shoud take into considerations possible collisions. 
+	/// Preferrably mapping should be 1:1.</remarks>
+	public interface ITypeConvention
+	{
+		/// <summary>Returns document type for given entity type.</summary>
+		string GetDocumentType(Type entityType);
+
+	    /// <summary>Returns entity type for given document type.</summary>
+	    Type GetEntityType(string documentType);
+	}
+}
---
+++ CouchDude.Core/Conventions/TypeConventionBase.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace CouchDude.Core.Conventions
+{
+	/// <summary>Type convention base class providing basic assembly scan and caching 
+	/// functionality.</summary>
+	public abstract class TypeConventionBase: ITypeConvention
+	{
+		private readonly IDictionary<Type, string> entity2DocMap = new Dictionary<Type, string>();
+		private readonly IDictionary<string, Type> doc2EntityMap = new Dictionary<string, Type>();
+
+		// ReSharper disable DoNotCallOverridableMethodsInConstructor
+		protected TypeConventionBase(params Assembly[] assembliesToScan)
+		{
+			foreach (var entityType in assembliesToScan.SelectMany(a => a.GetTypes()))
+			{
+				var documentType = ProcessType(entityType);
+				if (documentType != null)
+				{
+					entity2DocMap.Add(entityType, documentType);
+					doc2EntityMap.Add(documentType, entityType);
+				}
+			}
+		}
+		// ReSharper restore DoNotCallOverridableMethodsInConstructor
+
+		/// <summary>Returns document type for entity type if it's one.</summary>
+		protected abstract string ProcessType(Type type);
+
+		/// <summary>Checks if document type have been registered.</summary>
+		protected bool HaveRegistered(string documentType)
+		{
+			return doc2EntityMap.ContainsKey(documentType);
+		}
+		
+		/// <inheritdoc/>
+		public string GetDocumentType(Type entityType)
+		{
+			string documentType;
+			return !entity2DocMap.TryGetValue(entityType, out documentType)? null: documentType;
+		}
+
+		/// <inheritdoc/>
+		public Type GetEntityType(string documentType)
+		{
+			Type entityType;
+			return !doc2EntityMap.TryGetValue(documentType, out entityType) ? null : entityType;
+		}
+	}
+
+	/// <summary></summary>
+	public class TypeNameToCamelCaseConvention: TypeConventionBase
+	{
+		protected override string ProcessType(Type type)
+		{
+			
+		}
+	}
+}
