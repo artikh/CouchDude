e351272e0d3978e92099512f27f11cce529abe9f
Artem Tikhomirov <artem@tikhomirovs.ru>
2011-12-21 19:19:28 +0400

    JsonObjectComparier tests

--- Src/Core/Core/Utils/JsonObjectComparier.cs
+++ Src/Core/Core/Utils/JsonObjectComparier.cs
@@ -8,9 +8,15 @@ namespace CouchDude.Utils
 	public class JsonObjectComparier: 
 		IEqualityComparer<JsonObject>, IEqualityComparer<JsonValue>, IEqualityComparer<JsonArray>, IEqualityComparer<JsonPrimitive>
 	{
+		private const int InitialHashCode = 17;
+		private const int HashCodeDelta = 23;
+
 		/// <summary>Compares two <see cref="JsonObject"/> instances returning wether one is equal to another.</summary>
 		public bool Equals(JsonObject objectA, JsonObject objectB)
 		{
+			if (ReferenceEquals(objectA, objectB)) return true;
+			if (ReferenceEquals(objectA, null) || ReferenceEquals(null, objectB)) return false;
+
 			if (objectA.Count != objectB.Count)
 				return false;
 
@@ -33,30 +39,42 @@ namespace CouchDude.Utils
 		/// <summary>Returns a hash code for the specified <see cref="JsonObject"/> instance.</summary>
 		public int GetHashCode(JsonObject obj)
 		{
-			var hashCode = Int32.MinValue;
-			foreach (var property in obj)
-			{
-				hashCode ^= property.Key.GetHashCode();
-				hashCode ^= GetHashCode(property.Value);
-			}
+			var hashCode = InitialHashCode;
+			if(!ReferenceEquals(obj, null))
+				foreach (var property in obj)
+				{
+					hashCode *= HashCodeDelta + property.Key.GetHashCode();
+					hashCode *= HashCodeDelta + GetHashCode(property.Value);
+				}
 			return hashCode;
 		}
 
 		/// <summary>Compares two <see cref="JsonPrimitive"/> instances returning wether one is equal to another.</summary>
 		public bool Equals(JsonPrimitive primitiveA, JsonPrimitive primitiveB)
 		{
+			if (ReferenceEquals(primitiveA, primitiveB)) return true;
+			if (ReferenceEquals(primitiveA, null) || ReferenceEquals(null, primitiveB)) return false;
 			return Equals(primitiveA.Value, primitiveB.Value);
 		}
 
 		/// <summary>Returns a hash code for the specified <see cref="JsonPrimitive"/> instance.</summary>
 		public int GetHashCode(JsonPrimitive primitive)
 		{
-			return primitive.Value.GetHashCode();
+			return ReferenceEquals(primitive, null)? InitialHashCode: primitive.Value.GetHashCode();
 		}
 
 		/// <summary>Compares two <see cref="JsonValue"/> instances returning wether one is equal to another.</summary>
 		public bool Equals(JsonValue valueA, JsonValue valueB)
 		{
+			// if values are the same instance they are assumed equal
+			if (ReferenceEquals(valueA, valueB)) return true;
+
+			// if valueA is null 
+			if (ReferenceEquals(valueA, null))
+				return !ReferenceEquals(null, valueB) && valueB.JsonType == JsonType.Default;
+			if(ReferenceEquals(null, valueB))
+				return valueA.JsonType == JsonType.Default;
+
 			if (valueA.JsonType != valueB.JsonType)
 				return false;
 
@@ -80,6 +98,9 @@ namespace CouchDude.Utils
 		/// <summary>Returns a hash code for the specified <see cref="JsonValue"/> instance.</summary>
 		public int GetHashCode(JsonValue value)
 		{
+			if (ReferenceEquals(value, null))
+				return InitialHashCode;
+
 			switch (value.JsonType)
 			{
 				case JsonType.String:
@@ -100,6 +121,9 @@ namespace CouchDude.Utils
 		/// <summary>Compares two <see cref="JsonArray"/> instances returning wether one is equal to another.</summary>
 		public bool Equals(JsonArray arrayA, JsonArray arrayB)
 		{
+			if (ReferenceEquals(arrayA, arrayB)) return true;
+			if (ReferenceEquals(arrayA, null) || ReferenceEquals(null, arrayB)) return false;
+
 			if (arrayA.Count != arrayB.Count)
 				return false;
 
@@ -118,9 +142,10 @@ namespace CouchDude.Utils
 		/// <summary>Returns a hash code for the specified <see cref="JsonArray"/> instance.</summary>
 		public int GetHashCode(JsonArray array)
 		{
-			var hashCode = Int32.MinValue;
-			foreach (var item in array)
-				hashCode ^= GetHashCode(item);
+			var hashCode = InitialHashCode;
+			if (!ReferenceEquals(array, null))
+				foreach (var item in array)
+					hashCode *= HashCodeDelta * GetHashCode(item);
 			return hashCode;
 		}
 	}
---
+++ Src/Core/Tests/Unit/Utils/JsonObjectComparierTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Json;
+using System.Linq;
+using System.Text;
+using CouchDude.Utils;
+using Xunit;
+using Xunit.Extensions;
+
+namespace CouchDude.Tests.Unit.Utils
+{
+	public class JsonObjectComparierTest
+	{
+		private readonly JsonObjectComparier comparier = new JsonObjectComparier();
+
+		[Fact]
+		public void ShouldTreatNullArgumentsAreEqual() 
+		{
+			Assert.True(comparier.Equals(null, (JsonArray)null));
+			Assert.True(comparier.Equals(null, (JsonObject)null));
+			Assert.True(comparier.Equals(null, (JsonPrimitive)null));
+			Assert.True(comparier.Equals(null, (JsonValue)null));
+		}
+
+		[Fact]
+		public void ShouldTreatNullAndEmptyArrayAsUnEqual() 
+		{
+			Assert.False(comparier.Equals(null, new JsonArray()));
+			Assert.False(comparier.Equals(new JsonArray(), null));
+			Assert.False(comparier.Equals(null, new JsonArray() as JsonValue));
+			Assert.False(comparier.Equals(new JsonArray(), null as JsonValue));
+		}
+
+		[Fact]
+		public void ShouldTreatNullAndEmptyObjectAsUnEqual() 
+		{
+			Assert.False(comparier.Equals(null, new JsonObject()));
+			Assert.False(comparier.Equals(new JsonObject(), null));
+			Assert.False(comparier.Equals(null, new JsonObject() as JsonValue));
+			Assert.False(comparier.Equals(new JsonObject(), null as JsonValue));
+		}
+
+		[Fact]
+		public void ShouldTreatNullAndDefaultValueAsEqual()
+		{
+			var jsonObject = new JsonObject();
+			var defaultInstance = jsonObject.ValueOrDefault("bf2360599bb84e57a2f465f2aa7f5a1b");
+			Assert.True(comparier.Equals(null, defaultInstance));
+			Assert.True(comparier.Equals(defaultInstance, null));
+		}
+
+		[Fact]
+		public void ShouldTreatDifferentJsonValueTypesAsUnequal() 
+		{
+			Assert.False(comparier.Equals(new JsonArray(), new JsonObject()));	
+			Assert.False(comparier.Equals(new JsonPrimitive(42), new JsonArray()));
+			Assert.False(comparier.Equals(new JsonObject(), new JsonPrimitive(42)));	
+		}
+
+		[Fact]
+		public void SholudReturnFixedAsHashCodeOfNull() 
+		{
+			var hashCodeOfZero = comparier.GetHashCode((JsonArray) null);
+			Assert.Equal(hashCodeOfZero, comparier.GetHashCode((JsonObject)null));
+			Assert.Equal(hashCodeOfZero, comparier.GetHashCode((JsonValue)null));
+			Assert.Equal(hashCodeOfZero, comparier.GetHashCode((JsonPrimitive)null));	
+		}
+
+		[Theory]
+		[InlineData(@"{}")]
+		[InlineData(@"[]")]
+		[InlineData(@"[""test""]")]
+		[InlineData(@"[""""]")]
+		[InlineData(@"["""", 42]")]
+		[InlineData(@"{""key"": ""value"", ""flag"": false}")]
+		[InlineData(@"{""key"": ""value"", ""subobject"":{}}")]
+		[InlineData(@"{""key"": ""value"", ""subarray"":[1, 2, ""3""]}")]
+		[InlineData(@"{""key"": ""value"", ""subobject"":{""subkey"": 42}, ""subarray"":[1, 2, ""3""]}")]
+		public void ShouldReturnSameHashCodeForSameJson(string jsonString) 
+		{
+			var valueA = JsonValue.Parse(jsonString);
+			var valueB = JsonValue.Parse(jsonString);
+			Assert.True(comparier.Equals(valueA, valueB));
+			Assert.Equal(comparier.GetHashCode(valueA), comparier.GetHashCode(valueB));
+		}
+
+		[Fact]
+		public void ShouldTreatDifferentlySizedArraysAsUnequal() 
+		{
+			Assert.False(comparier.Equals(JsonValue.Parse("[1, 42]"), JsonValue.Parse("[1, 42, 4]")));	
+		}
+
+		[Fact]
+		public void ShouldTreatDifferentArraysAsUnequal() 
+		{
+			Assert.False(comparier.Equals(JsonValue.Parse("[1, 42]"), JsonValue.Parse("[1, 44]")));	
+		}
+	}
+}
